3b132d710f6bf1ad655f48e4402a1093461348ff
promise-temi <temi.promisejohn@gmail.com>
2025-05-11 17:57:23 +0200

    j'ai reussi a supprimer grace a mon jupiter notebook toutes les collones vides. entre temps je lai ajouter a ma bdd mongo et j'ai extrait les liens et tout ca manuellement. le dataset est si gros et si volumineux que je commence a me demander si je ne devrait pas prendre un dataset plus petit fourni par kaggle plutot que de le prendre directement d'openfoodfacts. Je n'ai clairement  pas les ressources materielle pour un si gros dataset. mon ordi m'a lacché plusieurs fois.

--- data/scripts/get_openfoodfacts_products.py
+++ data/scripts/get_openfoodfacts_products.py
@@ -1,22 +1,9 @@
-import requests
-import io
-import gzip
-import pandas as pd
+from modules.GetData import GetCsv
 
-def get_openfoodfacts_database():
 
-    # Récupération de la base de données Open Food Facts au format csv mais compressée en zip
-    print("Récupération du dataset de produits Open Food Facts...")
-    url = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz"
-    response = requests.get(url, stream=True)
+csv_url = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz"
+csv_filename = "../openfoodfacts_products.csv"
+parket_filename = "../openfoodfacts_products.parquet"
 
-    # Vérification du statut de la requête
-    if response.status_code == 200:
-        with open("../openfoodfacts_products.csv.gz", "wb") as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-        print("Téléchargement terminé avec succès.")
-    else:
-        print(f"Erreur : {response.status_code}")
-
-get_openfoodfacts_database()
+products = GetCsv(csv_url, csv_filename, True, True, parket_filename)
+products.get_csv_from_url()
