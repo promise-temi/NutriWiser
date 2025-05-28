81de88e0b579fad615ce09ec27c9882435250e23
promise-temi <temi.promisejohn@gmail.com>
2025-05-28 14:53:25 +0200

    j'ai utiliser une autre apporoche avec pyspark pour reccuperer les données pertinantes.

--- data/scripts/get_openfoodfacts_products.py
+++ data/scripts/get_openfoodfacts_products.py
@@ -1,9 +1,23 @@
-from modules.GetData import GetCsv
-
+import requests
+import polars as pl
 
 csv_url = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz"
-csv_filename = "../openfoodfacts_products.csv"
-parket_filename = "../openfoodfacts_products.parquet"
+response = requests.get(csv_url, stream=True)
 
-products = GetCsv(csv_url, csv_filename, True, True, parket_filename)
-products.get_csv_from_url()
+if response.status_code == 200:
+    with open("../openfoodfacts_products.csv.gz", "wb") as file:
+        for chunk in response.iter_content(chunk_size=8000):
+            file.write(chunk)
+    print("Télechargement du fichier réussi.")
+else:
+    print(f"Erreur lors du télechargement du fichier : {response.status_code}")
+
+
+
+df = pl.read_csv("../openfoodfacts_products.csv.gz", 
+                 separator="\t", 
+                 infer_schema_length=10_000, 
+                 quote_char=None,  
+                 ignore_errors=True)
+df.write_parquet("../openfoodfacts_products.parquet", compression="gzip")
+print("Conversion terminée !")
