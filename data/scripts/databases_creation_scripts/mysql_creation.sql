65682288048bae3cd1140eed089d5339c93b4818
promise-temi <temi.promisejohn@gmail.com>
2025-06-16 00:18:46 +0200

    ma base de données est prete et suffisamentt complete je peut passer a l'étape suivante. d'ailleur j'ai pu creer un script pour réccuperer les produits rappelée de l'api data.gouv. toutes les données pertinantes sont dans mongo!

--- data/scripts/databases_creation_scripts/mysql_creation.sql
+++ data/scripts/databases_creation_scripts/mysql_creation.sql
@@ -1,7 +1,11 @@
 CREATE TABLE toxicite (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    label VARCHAR(255) NOT NULL,
-    niveau INT NOT NULL
+    label VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE additifs (
@@ -12,6 +16,14 @@ CREATE TABLE additifs (
     description_avancee TEXT,
     remarques TEXT,
     FOREIGN KEY (toxicite_id) REFERENCES toxicite(id)
+);
+
+CREATE TABLE additifs_classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code_additif VARCHAR(50) NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY (code_additif) REFERENCES additifs(code),
+    FOREIGN KEY (class_id) REFERENCES classes(id)
 );
 
 CREATE TABLE additifs_noms (
